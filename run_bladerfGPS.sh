30de61f1afc7f7a8978a7284fc98169ce77f2081
JuhwanNoh <juwhan1019@hanmail.net>
2017-06-27 17:24:41 +0900

    run_bladerfGPS will work in 2017

--- run_bladerfGPS.sh
+++ run_bladerfGPS.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 year=`date -u +%Y`
+year2=${year:(-2)}
 day=`date -u +%j`
 
-brdc_file="brdc${day}0.16n"
-brdc_file_year="brdc${year}_${day}0.16n"
+brdc_file="brdc${day}0.${year2}n"
+brdc_file_year="brdc${year}_${day}0.${year2}n"
 
 if [ ! -e "$brdc_file_year" ]; then
 	wget ftp://cddis.gsfc.nasa.gov/gnss/data/daily/${year}/brdc/${brdc_file}.Z -O ${brdc_file_year}.Z
